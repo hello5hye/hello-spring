73ea0586573a1cf23d3851c5ac8254e3c896b90e
hye8253 <cocoa_s@naver.com>
2023-03-06 23:08:46 +0900

    initialize spring

---
+++ src/main/java/hello/hellospring1/repository/MemoryMemberRepository.java
@@ -0,0 +1,45 @@
+package hello.hellospring1.repository;
+
+import hello.hellospring1.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; // 키 값 생성해주는거
+
+
+    @Override
+    public Member save(Member member) {
+        // 아이디 셋팅
+        member.setId(++sequence);
+        // 스토어 저장
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name)) // 람다식으로
+                .findAny();
+
+    }
+
+    @Override
+    public List<Member> findAll() {
+
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
