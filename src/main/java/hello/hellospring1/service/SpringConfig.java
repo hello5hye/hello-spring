3b6808601df80d84b134f8b8e3a40ca76fba7d7c
hye8253 <cocoa_s@naver.com>
2023-06-15 16:18:29 +0900

    initialize spring

---
+++ src/main/java/hello/hellospring1/service/SpringConfig.java
@@ -0,0 +1,20 @@
+package hello.hellospring1.service;
+
+import hello.hellospring1.repository.MemberRepository;
+import hello.hellospring1.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
