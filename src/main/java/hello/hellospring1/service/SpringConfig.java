f62bf4e5ff390fe3144d4603a96d01b45294e09c
hye8253 <cocoa_s@naver.com>
2023-07-08 15:31:45 +0900

    spring Jdbc file add

--- src/main/java/hello/hellospring1/service/SpringConfig.java
+++ src/main/java/hello/hellospring1/service/SpringConfig.java
@@ -1,20 +1,30 @@
 package hello.hellospring1.service;
 
+import hello.hellospring1.repository.JdbcMemberRepository;
 import hello.hellospring1.repository.MemberRepository;
 import hello.hellospring1.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
 
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
+
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
+
 }
