0b3c618d1b6b6b6f7a1cfd41535da6c75109f209
hello5hye <hello.hyeseon@gmail.com>
2023-07-13 18:40:25 +0900

    JdbcTemplate, JPA learn

--- src/main/java/hello/hellospring1/service/SpringConfig.java
+++ src/main/java/hello/hellospring1/service/SpringConfig.java
@@ -1,21 +1,22 @@
 package hello.hellospring1.service;
 
-import hello.hellospring1.repository.JdbcMemberRepository;
-import hello.hellospring1.repository.MemberRepository;
-import hello.hellospring1.repository.MemoryMemberRepository;
+import hello.hellospring1.repository.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private DataSource dataSource;
+  private EntityManager em;
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-    @Bean
+  public SpringConfig(EntityManager em) {
+      this.em = em;
+  }
+
+
+  @Bean
     public MemberService memberService() {
 
         return new MemberService(memberRepository());
@@ -24,7 +25,11 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+    return new JpaMemberRepository(em);
     }
+
+
 
 }
--- src/test/java/hello/hellospring1/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring1/service/MemberServiceIntegrationTest.java
@@ -1,11 +1,14 @@
 package hello.hellospring1.service;
 
 import hello.hellospring1.domain.Member;
+import hello.hellospring1.repository.MemberRepository;
 import hello.hellospring1.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,23 +18,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @Transactional
 class MemberServiceIntegrationTest {
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
 
-
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-
-    }
-
+    @Autowired MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
+    @Commit
     void 회원가입() {
         // given
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring1");
 
         // when
         Long saveId = memberService.join(member);
@@ -55,25 +52,9 @@ class MemberServiceIntegrationTest {
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-/*
-        try {
-            memberService.join(member2);
-            fail();
-        } catch (IllegalStateException e) {
-            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-
-        }
-*/
 
 
         //then
     }
 
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
-    }
 }
